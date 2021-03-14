d8cd6d5fb7130383d5c1430543c758f2af9968ec
amerariia <polyanska.nastya@gmail.com>
2021-03-15 01:01:51 +0200

    Change Repositories, Add new query

--- src/main/java/com/example/springServer/repository/ChatRepository.java
+++ src/main/java/com/example/springServer/repository/ChatRepository.java
@@ -1,11 +1,14 @@
 package com.example.springServer.repository;
 
 import com.example.springServer.entity.Chat;
+import com.example.springServer.entity.RoleEntity;
 import com.example.springServer.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.annotation.Secured;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Integer> {
     List<Chat> findAllByUsersContains(User user);
+    void deleteAllByCreator_RoleNot(RoleEntity roleEntity);
 }
--- src/main/java/com/example/springServer/repository/UserRepository.java
+++ src/main/java/com/example/springServer/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.springServer.repository;
 
+import com.example.springServer.entity.Group;
 import com.example.springServer.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
